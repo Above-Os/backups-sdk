9d2e52da0e933a1009bae8daeb997fc5c126de74
aby913 <aby913@163.com>
2025-03-28 17:36:15 +0800

    fix: init log

--- client.go
+++ client.go
@@ -1,15 +1,25 @@
 package backupssdk
 
 import (
+	"path"
+
 	"bytetrade.io/web3os/backups-sdk/cmd/backup"
 	"bytetrade.io/web3os/backups-sdk/cmd/download"
 	"bytetrade.io/web3os/backups-sdk/cmd/region"
 	"bytetrade.io/web3os/backups-sdk/cmd/restore"
 	"bytetrade.io/web3os/backups-sdk/cmd/snapshots"
+	"bytetrade.io/web3os/backups-sdk/pkg/constants"
+	"bytetrade.io/web3os/backups-sdk/pkg/logger"
+	"bytetrade.io/web3os/backups-sdk/pkg/utils"
 	"github.com/spf13/cobra"
 )
 
 func NewBackupCommands() *cobra.Command {
+	var homeDir = utils.GetHomeDir()
+	var jsonLogDir = path.Join(homeDir, constants.DefaultBaseDir, constants.DefaultLogsDir)
+
+	logger.InitLogger(jsonLogDir, true)
+
 	cmds := &cobra.Command{
 		Use:   "backups",
 		Short: "Olares backup tool-kit",
